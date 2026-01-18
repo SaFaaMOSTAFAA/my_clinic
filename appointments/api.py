a70cbf8acf9590ce313bb0346e12fc634d6a943f
safaamostafa <safamos21@gmail.com>
2026-01-18 23:44:30 +0200

    add list serializers to vist and appointment serializers

--- appointments/api.py
+++ appointments/api.py
@@ -1,9 +1,13 @@
 from rest_framework.viewsets import ModelViewSet
 
 from appointments.models import Appointment
-from appointments.serializers import AppointmentSerializer
+from appointments.serializers import AppointmentSerializer, ListAppointmentSerializer
 
 
 class AppointmentViewSet(ModelViewSet):
     queryset = Appointment.objects.all()
-    serializer_class = AppointmentSerializer
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return ListAppointmentSerializer
+        return AppointmentSerializer
