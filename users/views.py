81881f36d052edeb0dd10f004d16c0a0256d9260
safaamostafa <safamos21@gmail.com>
2026-01-26 14:55:51 +0200

    solve users admin

--- users/views.py
+++ users/views.py
@@ -1,7 +1,5 @@
-from django.shortcuts import redirect, render
 from rest_framework.viewsets import ModelViewSet
 
-from users.forms import PatientForm
 from users.models import Doctor, Patient
 from users.serializers import DoctorSerializer, PatientSerializer
 
@@ -14,15 +12,3 @@ class DoctorViewSet(ModelViewSet):
 class PatientViewSet(ModelViewSet):
     queryset = Patient.objects.all()
     serializer_class = PatientSerializer
-
-
-def create_patient(request):
-    if request.method == 'POST':
-        form = PatientForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('success')
-    else:
-        form = PatientForm()
-
-    return render(request, 'users/patient_form.html', {'form': form})
